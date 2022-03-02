1c2b7d30bf7478c9323c11217d10b3d18d571afe
goldPHANTOMyt <90315139+goldPHANTOMyt@users.noreply.github.com>
2022-03-02 19:14:24 +0530

    Add files via upload

---
+++ Racial Tolerance Test.py
@@ -0,0 +1,16 @@
+import time
+Name=input("Write your name here:")
+print ("Hi! Nigga I think your name is",(Name))
+time.sleep(3)
+print ("BTW",(Name),"If ur a asian just try to not cry after the exams when you still got 99 out of 100")
+time.sleep(3)
+print ("Nigga")
+time.sleep(3)
+print ("Ha black people go brrrrrr")
+time.sleep(3)
+b=('yes')
+a=input("If you were triggered then write yes or else write no : ")
+if b == a:
+    print ("Ha",(Name),"Ur still a nigger")
+else:
+    print ("Ok!",(Name),"you have good racial tolerance")
